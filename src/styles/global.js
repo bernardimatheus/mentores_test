a0c427fa3614a9ba81d4b5a09e01967c194ab67e
bernardi <bernardi.matheus@gmail.com>
2019-07-23 21:02:38 -0300

    Using API and implement Redux

--- src/styles/global.js
+++ src/styles/global.js
@@ -11,7 +11,7 @@ export default createGlobalStyle`
   }
 
   body {
-    background: url(${background}) no-repeat center top;
+    background: #46A4D9 url(${background}) no-repeat center top;
     -webkit-font-smoothing: antialiased;
   }
 
